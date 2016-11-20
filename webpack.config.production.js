1ea1725cf9bf9d83c7cd1f5573ef45c195dfd4de
Daichi Kimura <dkimura@bdd.xyz>
2016-11-20 15:57:18 +0900

    chore: Add Webpack Configs

---
+++ webpack.config.production.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: 'production',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'assets', 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  devtool: '#source-map',
+};
