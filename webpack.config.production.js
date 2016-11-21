5c80e92bf2fba6293937dffeb4e5a73194074f9b
Daichi Kimura <dkimura@bdd.xyz>
2016-11-21 22:36:05 +0900

    fix: production build

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -13,7 +13,7 @@ module.exports = {
     new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: 'production',
+        NODE_ENV: '"production"',
       },
     }),
     new webpack.optimize.UglifyJsPlugin({
