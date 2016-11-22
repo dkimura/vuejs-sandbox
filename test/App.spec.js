5105566e67070e106835fadc0aa09f2f24254d95
Daichi Kimura <dkimura@bdd.xyz>
2016-11-23 04:39:43 +0900

    style: adjust spec code layout

--- test/App.spec.js
+++ test/App.spec.js
@@ -3,11 +3,11 @@ import Vue from 'vue';
 import App from '../src/App.vue';
 
 describe('App.vue', () => {
-  it('should render content', () => {
-    const vm = new Vue({
-      render: h => h(App),
-    }).$mount();
+  const vm = new Vue({
+    render: h => h(App),
+  }).$mount();
 
-    assert(vm.$el.textContent, 'hello Pug!!!');
+  it('should render content', () => {
+    assert.equal(vm.$el.textContent, 'hello Pug!!!');
   });
 });
